a4a6c471f76c239ca9fa1df3cf001f8b09ec5cac
Jorge Lonzo <jlonzo@gmail.com>
2023-10-12 14:02:50 -0500

    Changes to devcontainers file

--- 02 Functions and Variables/ProblemSets/faces.py
+++ 02 Functions and Variables/ProblemSets/faces.py
@@ -2,9 +2,11 @@ def main():
     MyString = str(input("Input your String: "))
     print(convert(MyString))
 
+
 def convert(toConvert):
-    toConvert = toConvert.replace(":)","🙂")
-    toConvert = toConvert.replace(":(","🙁")
+    toConvert = toConvert.replace(":)", "🙂")
+    toConvert = toConvert.replace(":(", "🙁")
     return toConvert
+
 
 main()
